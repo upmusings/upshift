74eba87c54ec0580147e63ae120aa10e1b2e0631
Pete Schaffner <pjschaffner@gmail.com>
2013-06-20 13:31:15 -0400

    Fixed logo animation & added some images

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
 $(function () {
 
+  // retina images
+  $('.work img').retina('@2x');
+
+  // setup image sliders
+  $('.slides').slidesjs({
+    width: 760,
+    height: 640,
+    navigation: false,
+    pagination: {
+      effect: 'fade'
+    },
+    effect: {
+      fade: {
+        speed: 400,
+        crossfade: true
+      }
+    }
+  });
+
   // orchestrate apple fall
   $('#apple').delay(700)
     .animate({ top: '78px' }, 400, 'easeOutBounce', function() {
       setTimeout(function() {
         $('h1').css('opacity', 1);
+
+        setTimeout(function() {
+          $('#main').css('opacity', 1);
+          // unfix header
+          $('#header').removeClass('fixed');
+        }, 1500);
       }, 400);
     });
 
